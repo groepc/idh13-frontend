c1a531099eff49cfdd4e622e7dd7ecca3c08c977
Peter Janssens <Peter Janssens>
2016-11-05 15:41:01 +0100

    let static files pass through

--- web/index.php
+++ web/index.php
@@ -3,6 +3,11 @@
 // web/index.php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
